031b149dc59ece55a48adc702698ab1ec2cd23ba
unknown <fercris1124@gmail.com>
2026-09-03 17:57:22 -0500

    Proyecto inicial

---
+++ src/test/java/co/sgp/SgpApplicationTests.java
@@ -0,0 +1,13 @@
+package co.sgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
